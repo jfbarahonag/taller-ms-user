9c4eb6171b5bf8783532b4e2f0011b2dc6230b47
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2024-08-15 01:34:58 +0000

    user entity has been created

---
+++ src/main/java/com/ms/user/model/UserEntity.java
@@ -0,0 +1,25 @@
+package com.ms.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="user")
+public class UserEntity {
+
+    @Id
+    private String id;
+    private String username;
+    private String document;
+    private String documentType;
+
+}
