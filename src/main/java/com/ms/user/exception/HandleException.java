1a2517af4f4a794d9261116c6f1eb3a53f10af9d
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2024-08-16 02:17:08 +0000

    exception handler has been created and method to handle validation errors has been created

---
+++ src/main/java/com/ms/user/exception/HandleException.java
@@ -0,0 +1,30 @@
+package com.ms.user.exception;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+@AllArgsConstructor
+public class HandleException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getObjectName() + " : " + err.getField() + " : " + err.getDefaultMessage())
+                .toList();
+        
+        log.error("Validation error(s):\n{}" + errors);
+        
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
